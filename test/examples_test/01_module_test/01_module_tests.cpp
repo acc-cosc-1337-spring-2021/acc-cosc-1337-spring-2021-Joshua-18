1319351ea65357edeed149479bf999611409cd6a
Joshua-18 <joshua.membreno@g.austincc.edu>
2021-02-03 21:54:08 -0600

    test module 03_input passed

--- test/examples_test/01_module_test/01_module_tests.cpp
+++ test/examples_test/01_module_test/01_module_tests.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "vars.h"
+#include "input.h"
 #include<string>
 
 TEST_CASE("Verify Test Configuration", "verification") {
@@ -11,6 +12,12 @@ TEST_CASE("Verify Test Configuration", "verification") {
 TEST_CASE("Verify the echo variable", "verify function param is returned") {
 	REQUIRE(echo_variable(5) == 5);
 	REQUIRE(echo_variable(10) == 10);
+}
+
+TEST_CASE("Verify get total function", "returns product of units * rate") 
+{
+	REQUIRE(get_total(5, 10) == 50);
+	REQUIRE(get_total(6, 6) == 36);
 }
 
 /*test case add_to_double_1 with 0 as parameter*/
