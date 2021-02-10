d01f9baa9e61b76d6214d4c995f47e79fe3ee656
Joshua-18 <joshua.membreno@g.austincc.edu>
2021-02-10 17:09:39 -0600

    casting, overflow and constants examples

--- src/examples/02_module/04_constants/main.cpp
+++ src/examples/02_module/04_constants/main.cpp
@@ -1,5 +1,15 @@
+#include<iostream>
+#include "constants.h"
+
+using std::cout; using std::cin;
 
 int main()
 {
+    auto radius = .01;
+    cout<<"Enter radius of a circle: ";
+    cin>>radius;
+
+    cout<<"Area of cicle is: "<<get_area_of_circle(radius);
+    
     return 0;
 }
