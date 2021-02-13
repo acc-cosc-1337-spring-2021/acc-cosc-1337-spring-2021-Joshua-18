dd09fc8b75b007051b37fcb51ebeb4327fafa1dc
Joshua-18 <joshua.membreno@g.austincc.edu>
2021-02-12 21:27:56 -0600

    homework 2 Restaurant Bill

--- src/homework/01_variables/variables.cpp
+++ src/homework/01_variables/variables.cpp
@@ -5,10 +5,23 @@ int add_numbers(int num1, int num2)
 {
 	return num1 + num2;
 }
-
 //write code for multiply_numbers function here
 //function returns product of num1 and num2
 
 
 
+//const double taxt_rate
+const double taxt_rate = 0.0675;
 
+// function multiplies taxt rate * meal amount and returns their product
+double get_sales_tax_amount(double meal_amount)
+{
+	return taxt_rate * meal_amount;
+	 
+}
+
+// func code meal_amount * tip_rate
+double get_tip_amount(double meal_amount, double tip_rate)
+{
+	return meal_amount * tip_rate;
+}
--- test/homework_test/01_variables_test/01_variables_tests.cpp
+++ test/homework_test/01_variables_test/01_variables_tests.cpp
@@ -12,3 +12,17 @@ TEST_CASE("Verify sum_numbers function")
 	REQUIRE(add_numbers(10, 10) == 20);
 }
 
+
+// TEST CASE #1 Get Sales Tax
+TEST_CASE("Test Get Sales Tax") 
+{
+	REQUIRE(get_sales_tax_amount(10) == 0.675);
+	REQUIRE(get_sales_tax_amount(20) == 1.35);
+}
+
+// TEST CASE #2 Get Tip Amount. 
+TEST_CASE("Test Get Tip Amount. ") 
+{
+	REQUIRE(get_tip_amount(20, 0.15) == 3);
+	REQUIRE(get_tip_amount(20, 0.20) == 4);
+}
