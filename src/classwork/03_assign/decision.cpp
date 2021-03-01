05d36925a20248d8ce71cdec51dd63fec1519cd4
Joshua-18 <joshua.membreno@g.austincc.edu>
2021-02-28 18:19:00 -0600

    Homework 3

--- src/classwork/03_assign/decision.cpp
+++ src/classwork/03_assign/decision.cpp
@@ -1,1 +1,61 @@
 //cpp
+#include <string>
+#include <decision.h>
+
+// USING IF STATEMENTS
+std::string get_letter_grade_using_if(int grade)
+{
+    if (grade >= 90 && grade <= 100)
+    {
+        return "A";
+    }
+    else if (grade >= 80 && grade <= 89)
+    {
+        return "B";
+    }
+    else if (grade >= 70 && grade <= 79)
+    {
+        return "C";
+    }
+    else if (grade >= 60 && grade <= 69)
+    {
+        return "D";
+    }
+    else if (grade >= 00 && grade <=59)
+    {
+        return "F";
+    }
+    else
+    {
+        return "INVALID GRADE";
+    }
+}
+
+// USING SWITCH CASES
+std::string get_letter_grade_using_switch(int grade)
+{
+    std::string string_grade;
+
+    switch (grade)
+    {
+    case 90 ... 100 :
+        string_grade = "A";
+        break;
+    case 80 ... 89 :
+        string_grade = "B";
+        break;
+    case 70 ... 79 :
+        string_grade = "C";
+        break;
+    case 60 ... 69 :
+        string_grade = "D";
+        break;
+    case 00 ... 59 :
+        string_grade = "F";
+        break;
+    default:
+        string_grade = "Invalid Grade!";
+        break;
+    }
+    return string_grade;
+}
--- src/classwork/03_assign/main.cpp
+++ src/classwork/03_assign/main.cpp
@@ -1,10 +1,23 @@
 //Write the include statement for decisions.h here
-
+#include "decision.h"
 //Write namespace using statements for cout and cin
-
+#include<iostream>
+using std::cout;		using std::cin;		using namespace std;
 int main() 
 {
+	int grade;
+	cout<<"Please enter your grade in numbers: ";
+	cin>>grade;
 
+	if (grade >= 0 && grade <= 100)
+	{
+		cout<<"Your grade of "<<grade<<" using if statement is: "<<get_letter_grade_using_if(grade)<<"\n";
+		cout<<"Your grade of "<<grade<<" using switch case is: "<<get_letter_grade_using_switch(grade)<<"\n";
+	}
+	else
+	{
+		cout<<"Invalid input";
+	}
 	return 0;
 }
 
