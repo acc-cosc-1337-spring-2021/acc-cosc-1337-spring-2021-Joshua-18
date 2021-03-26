a63d8ecaeb2db7bfd96785eef2b2f46eeaa209f7
Joshua-18 <joshua.membreno@g.austincc.edu>
2021-03-25 21:06:00 -0500

    Bank account deposit/withdraw w test

--- src/examples/06_module/01_bank/main.cpp
+++ src/examples/06_module/01_bank/main.cpp
@@ -1,6 +1,15 @@
+#include<iostream>
+using std::cout;
+#include "bank_account.h"
 
 int main()
 {
+	// string name;// creates a string var in memory
+	// 			//create an instance of an object
+
+	BankAccount account;//variable-an instance of BankAccount object
+	cout<<"Balance: "<<account.get_balance()<<"\n";
+
 
 	return 0;
 }
