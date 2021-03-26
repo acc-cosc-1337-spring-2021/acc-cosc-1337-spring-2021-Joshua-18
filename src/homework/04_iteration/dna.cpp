753134d212853762e58922eaf4e29ad913f1afaa
Joshua-18 <joshua.membreno@g.austincc.edu>
2021-03-26 01:18:42 -0500

    Homework 5

--- src/homework/04_iteration/dna.cpp
+++ src/homework/04_iteration/dna.cpp
@@ -6,7 +6,20 @@ Calculate GC content:
 Iterate string count Gs and Cs, divide count by string length.
 Return quotient.
 */
+double get_gc_content(const string& dna)
+{
+    double g_string_count = 0;
+    double c_string_count = 0;
 
+    for (int i = 0; i < dna.length(); i++)
+    {
+        if (dna[i] == 'G')
+            g_string_count++;
+        if (dna[i] == 'C')
+            c_string_count++;
+    }
+    return (g_string_count + c_string_count) / dna.length();
+}
 
 
 
@@ -14,7 +27,17 @@ Return quotient.
 Write code for function get_reverse_string that
 accepts a string parameter and returns a string reversed.
 */
-
+string get_reverse_string(string dna)
+{
+    string reversed_dna_str = "";
+    for (int i = dna.length() - 1; i >= 0; i--)
+    {
+        reversed_dna_str += dna[i];
+        
+        
+    }
+    return reversed_dna_str;
+}
 
 
 /*
@@ -27,4 +50,27 @@ b. iterate local string variable and
 c. return string
 
 */
+string get_dna_complement(string dna)
+{
+    string results = get_reverse_string(dna);
 
+    for (int i = 0; i < results.length(); i++)
+    {
+        switch (results[i])
+        {
+        case 'A':
+            results[i] = 'T';
+            break;
+        case 'T':
+            results[i] = 'A';
+            break;
+        case 'C':
+            results[i] = 'G';
+            break;
+        case 'G':
+            results[i] = 'C';
+            break;
+        }
+    }
+    return results;
+}
--- src/homework/04_iteration/main.cpp
+++ src/homework/04_iteration/main.cpp
@@ -1,7 +1,9 @@
 //write include statements
-
+#include "dna.h"
+#include <iostream>
+#include<string>
 //write using statements
-
+using std::cout; using std::cin; using std::string;
 
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
@@ -12,5 +14,42 @@ user enters a y or Y.
 */
 int main() 
 {
+	char choice_loop;
+	string gc_choice;
+	string dna;
+
+	do
+	{	
+		cout<<"\n";
+		cout<<"Enter 1 to get GC content or\n";
+		cout<<"Enter 2 to get DNA complement"<<"\n";
+
+		cout<<"Enter your choice: ";
+		cin>>gc_choice;
+		
+			if (gc_choice == "1")
+			{
+				cout<<"Enter a DNA string in caps: ";
+				cin>>dna;
+				cout<<"Your DNA strig as a GC content is: "<<get_gc_content(dna)<<"\n";
+			}
+			else if (gc_choice == "2")
+			{
+				cout<<"Enter a DNA string in caps: ";
+				cin>>dna;
+				cout<<"Your DNA strig as a DNA complement is: "<<get_dna_complement(dna)<<"\n";
+			}
+			else
+			{
+				cout<<"Error: invalid choice!"<<"\n";
+			}
+		
+			cout<<"Run this program again? (Enter Y or y): ";
+			cin>>choice_loop;
+
+		
+	} while (choice_loop == 'Y' || choice_loop == 'y');
+	
+
 	return 0;
 }
