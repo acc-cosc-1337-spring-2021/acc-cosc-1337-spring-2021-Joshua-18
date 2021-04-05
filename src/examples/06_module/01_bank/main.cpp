24d5633c24448185a96a675c8f1d811112d31ccc
Joshua-18 <joshua.membreno@g.austincc.edu>
2021-04-04 22:16:19 -0500

    atm, explicit, friend class, operator overload

--- src/examples/06_module/01_bank/main.cpp
+++ src/examples/06_module/01_bank/main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
-using std::cout;
+using std::cout; using std::cin;
 
+#include "atm.h"
 #include "bank_account.h"
 
 int main()
@@ -8,24 +9,32 @@ int main()
 	// string name;// creates a string var in memory
 	// 			//create an instance of an object
 
-	BankAccount account = get_account(100);//variable-an instance of BankAccount object
-
-	display_balance(account);
-
-	auto balance = account.get_balance();
+	BankAccount a1(100);
+	BankAccount a2(200);
+	BankAccount account = a1 + a2;//get_account(100);//variable-an instance of BankAccount object
+	cin>>account;
+	/*char choice;
 	
-	cout<<"Balance: "<<balance<<"\n";
+	do
+	{
+		ATM atm;
+		atm.scan_card();
+		atm.display_balance();
+		cout<<"Continue? y or n";
+		cin>>choice;
+	}while(choice == 'y' || choice == 'Y');*/
+
+	cout<<account;
+	friend_display_balance(account);
+	display_account(account);
 
 	cout<<"Deposit $50\n";
 	account.deposit(50);
-
-	balance = account.get_balance();
-	cout<<"Balance: "<<balance<<"\n\n";
+	cout<<"Balance: "<<account.get_balance()<<"\n";
 
 	cout<<"Withdraw $10\n";
 	account.withdraw(10);
-	balance = account.get_balance();
-	cout<<"Balance: "<<balance<<"\n";
+	cout<<"Balance: "<<account.get_balance()<<"\n";
 	
 	return 0;
 }
