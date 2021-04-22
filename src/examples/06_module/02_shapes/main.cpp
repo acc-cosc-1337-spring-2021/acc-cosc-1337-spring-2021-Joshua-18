a6cbb77491bdd2d8fe9ccfde2dd144c90f63c617
Joshua-18 <joshua.membreno@g.austincc.edu>
2021-04-22 18:35:10 -0500

    Array of circle objects

--- src/examples/06_module/02_shapes/main.cpp
+++ src/examples/06_module/02_shapes/main.cpp
@@ -10,9 +10,19 @@ Create a Shape pointer of type Line
 Create vector of Shape pointers
 iterate with auto
 */
+#include<memory>
+using std::unique_ptr; using std::make_unique;
+#include<vector>
+using std::vector;
 
 int main() 
 {
+	const int SIZE = 3;
+	Circle circles[SIZE];//CREATE a circles array
+
+	circles[0].draw();
+	circles[1].draw();
+	circles[2].draw();
 
 	return 0;
 }
